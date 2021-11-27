bd416b435f7352aa16b20cf7b51c4ac6e294f45b
Júlia Mazáková <mazakovajulia@gmail.com>
2021-11-27 22:13:40 +0100

    symtable fixes

--- src/ST_stack.c
+++ src/ST_stack.c
@@ -2,8 +2,9 @@
 #include "error.h"
 
 /**
-  * @desc acces to the top of ST_stack
-  * @param  ST_stack *scope - current scope's visibility
+  * Acces to the top of ST_stack
+  * @param  ST_stack *scope -> current scope's visibility
+  * @return top symbol table
 */
 sym_tab_t *top_table(ST_stack *scope)
 {
@@ -15,23 +16,21 @@ sym_tab_t *top_table(ST_stack *scope)
         return NULL;
 }
 /**
-  * @desc push another local table into the stack
+  * Push another local table into the stack
   * @param  ST_stack **scope - current scope's visibility
-  * @return nothing
+  * @return true if success
 */
 bool push(struct ST_stack **scope)
 {
     ST_stack *new = NULL;
     sym_tab_t *table = sym_tab_init();
     new = (ST_stack *)malloc(sizeof(ST_stack));
-    // check for possible stack overflow
+    // check if stack was allocated correctly
     if (!new)
     {
         return false;
     }
-    
-    if(!*scope)
-    new->next = NULL;
+    // if we are pushing first symbol table (aka global)
     new->next = *scope;
     new->localtable = table;
     *scope = new;
@@ -39,13 +38,14 @@ bool push(struct ST_stack **scope)
 }
 
 /**
-  * @desc pop the top of ST_stack
+  * Pop the top of ST_stack
   * @param  ST_stack *scope - current scope's visibility
-  * @return nothing
+  * @return bool if success
 */
 bool pop(ST_stack **scope)
 {
     ST_stack *tmp;
+    // check if scope is empty
     if (*scope == NULL)
     {
         return false;
@@ -57,27 +57,101 @@ bool pop(ST_stack **scope)
     return true;
 }
 /**
-  * @desc search for item everywhere (local -> global)
-  * @param  ST_stack *scope - current scope's visibility
-  * @param  sym_tab_key_t key - key we want to find 
-  * @return nothing
+  * Search for item everywhere (local -> global)
+  * @param  ST_stack **scope - current scope's visibility
+  * @param  sym_tab_key_t key - key we want to find
+  * @return found item or NULL if item doesn't exist
 */
-sym_tab_item_t *scope_search(ST_stack** scope ,sym_tab_key_t key)
+sym_tab_item_t *scope_search(ST_stack **scope, sym_tab_key_t key)
 {
-     ST_stack *top = *scope;
-     if(top == NULL)
-     {
-      printf("\nStack is Empty!!!\n");
-      return NULL;
-     }
-   else{
-      ST_stack *temp = top;
-      while(temp != NULL){
-     sym_tab_item_t*tmp = NULL;
-	 if((tmp = sym_tab_find_in_table(temp->localtable,key)))
-     return tmp;
-	 temp = temp->next;
-      }
-      return NULL;
-   }
+    ST_stack *top = *scope;
+    // if symbol table is empty
+    if (top == NULL)
+    {
+        return NULL;
+    }
+    else
+    {
+        ST_stack *temp = top;
+        while (temp != NULL)
+        {
+            sym_tab_item_t *tmp = NULL;
+            if ((tmp = sym_tab_find_in_table(temp->localtable, key)))
+                return tmp;
+            temp = temp->next;
+        }
+        return NULL;
+    }
+}
+
+/**
+  * Initialize stack
+  * @return initialized stack
+*/
+ST_stack *init_ST_stack()
+{
+    ST_stack *scope = NULL;
+    return scope;
+}
+
+/**
+  * Free all remaining stacks
+  * @param ST_stack **scope -> current scope's visibilty
+  * @return true if succesfuly freed
+*/
+bool free_ST_stack(ST_stack **scope)
+{
+    ST_stack *top = *scope;
+    // check for empty symbol table
+    if (top == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        // loop through symtables
+        while (top != NULL)
+        {
+            pop(&top);
+        }
+        return true;
+    }
+    return false;
+}
+/**
+  * Find out if item is function
+  * @param  sym_tab_t *t - current symbol table
+  * @param  sym_tab_key_t key - key we want to check
+  * @return true if it's function
+*/
+bool isfunc(ST_stack **scope, sym_tab_key_t key)
+{
+    if (scope_search(scope, key) == NULL)
+        return false;
+    else
+    {
+        if (scope_search(scope, key)->data.item_type == HT_FUNC)
+            return true;
+        else
+            return false;
+    }
+}
+/**
+  * Find out if item is a variable
+  * @param  sym_tab_t *t - current symbol table
+  * @param  sym_tab_key_t key - key we want to check
+  * @bool  - true if it's variable
+*/
+bool isvar(ST_stack **scope, sym_tab_key_t key)
+{
+
+    if (scope_search(scope, key) == NULL)
+        return false;
+    else
+    {
+        if (scope_search(scope, key)->data.item_type == HT_VAR)
+            return true;
+        else
+            return false;
+    }
 }
--- src/symtable.h
+++ src/symtable.h
@@ -3,69 +3,74 @@
 
 #ifndef SYMTABLE_H
 #define SYMTABLE_H
-//This is the optimal value because it creates the largest possible hash table size and limits hashing collisions
-#define MAX 65535
+// This is the optimal value because it creates the largest possible hash table size and limits hashing collisions
+#define MAX 5
 
-#include <string.h>     
-#include <stdbool.h>    
+#include <string.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include "scanner.h"
 #include "error.h"
 
 // symtable item type
-typedef enum{
+typedef enum
+{
     HT_VAR,
     HT_FUNC,
     HT_OTHERS
 } sym_tab_item_type;
 
 // possible data types
-typedef enum {
-INTEGER,
-NUMBER,
-STRING,
-NIL
-}sym_tab_datatype;
+typedef enum
+{
+    INTEGER,
+    NUMBER,
+    STRING,
+    NIL
+} sym_tab_datatype;
 
 // Hash table structures:
-struct sym_tab;   
-typedef struct sym_tab sym_tab_t;  
+struct sym_tab;
+typedef struct sym_tab sym_tab_t;
 
 // DATA types
-typedef const char *sym_tab_key_t;       
-typedef int sym_tab_count_t;   
-typedef bool sym_tab_defined_t;   
-typedef bool sym_tab_declared_t;        
+typedef const char *sym_tab_key_t;
+typedef int sym_tab_count_t;
+typedef bool sym_tab_defined_t;
+typedef bool sym_tab_declared_t;
 typedef int num_of_params;
 
-//linked list of data types
-typedef struct datatypes_list {
+// linked list of data types
+typedef struct datatypes_list
+{
     sym_tab_datatype datatype;
     struct datatypes_list *next;
- }datatypes_list;
+} datatypes_list;
 
-
-typedef struct datatypes_list *data_type; 
+// linked list structure
+typedef struct datatypes_list *data_type;
 
 // data of item in table
-typedef struct  {   
-    sym_tab_item_type  item_type; 
-    data_type     return_data_types;
-    data_type     param_data_types;
-    sym_tab_defined_t  defined;
-    sym_tab_declared_t declared;
-    num_of_params   params;
-} sym_tab_data_t;                
+typedef struct
+{
+    sym_tab_item_type item_type; // type of item
+    data_type return_data_types; // return data types of functions and variables
+    data_type param_data_types;  // data types of parameters
+    sym_tab_defined_t defined;   // bool defined
+    sym_tab_declared_t declared; // bool declared
+    num_of_params params;        // number of parameters
+} sym_tab_data_t;
 
-//structure of item in table
-typedef struct sym_tab_item_t {  
-sym_tab_key_t          key;
-sym_tab_data_t         data;
-struct sym_tab_item_t *next;
-}sym_tab_item_t;
+// structure of item in table
+typedef struct sym_tab_item_t
+{
+    sym_tab_key_t key;
+    sym_tab_data_t data;
+    struct sym_tab_item_t *next;
+} sym_tab_item_t;
 
-//structure of table
+// structure of table
 struct sym_tab
 {
     size_t size;
@@ -77,20 +82,20 @@ struct sym_tab
 size_t sym_tab_hash_function(sym_tab_key_t str);
 
 // functions within sym_table:
-sym_tab_t *sym_tab_init();                                // constuctor of hasthable
-sym_tab_t *sym_tab_move(sym_tab_t *from);                    // move data to new hassym_table
-size_t sym_tab_size(const sym_tab_t * t);                 // number of items in hassym_table
-sym_tab_item_t * sym_tab_find_in_table(sym_tab_t *t, sym_tab_key_t key); // find item in hassym_table
-sym_tab_item_t * sym_tab_add_item(sym_tab_t *t, sym_tab_key_t key);
-bool sym_tab_add_data_function(sym_tab_item_t *item,data_type return_data_types,data_type param_data_types,sym_tab_declared_t dec,sym_tab_defined_t def,int par);
-bool sym_tab_add_data_var(sym_tab_item_t *item,data_type param_data_types,sym_tab_declared_t dec,sym_tab_defined_t def);
-bool sym_tab_erase(sym_tab_t * t, sym_tab_key_t key);         // delete item
-void sym_tab_for_each(const sym_tab_t * t, void (*f)(sym_tab_item_t *item)); // make action for every item in hassym_table
-void sym_tab_clear(sym_tab_t * t);                        // delete all items
-void sym_tab_free(sym_tab_t * t);                         // destructor of hassym_table
-bool isfunc(sym_tab_t *t, sym_tab_key_t key);                        //check if item is function
-bool isvar(sym_tab_t *t,sym_tab_key_t key);   
-data_type create_data_type(sym_tab_datatype first);  
-data_type add_data_type(data_type first, sym_tab_datatype value);
-void delete_data_types(data_type first);
-#endif // __sym_tab_H__
+sym_tab_t *sym_tab_init();                                              // constuctor of hasthable
+sym_tab_t *sym_tab_move(sym_tab_t *from);                               // move data to new hassym_table
+size_t sym_tab_size(const sym_tab_t *t);                                // number of items in hassym_table
+sym_tab_item_t *sym_tab_find_in_table(sym_tab_t *t, sym_tab_key_t key); // find item in hassym_table
+sym_tab_item_t *sym_tab_add_item(sym_tab_t *t, sym_tab_key_t key);      // create new item to add to symbol table
+// add data of function
+bool sym_tab_add_data_function(sym_tab_item_t *item, data_type return_data_types, data_type param_data_types, sym_tab_declared_t dec, sym_tab_defined_t def, int par);
+// add data of variable
+bool sym_tab_add_data_var(sym_tab_item_t *item, data_type return_data_types, sym_tab_declared_t dec, sym_tab_defined_t def);
+bool sym_tab_erase(sym_tab_t *t, sym_tab_key_t key);                        // delete item
+void sym_tab_for_each(const sym_tab_t *t, void (*f)(sym_tab_item_t *item)); // make action for every item in symbol table
+void sym_tab_clear(sym_tab_t *t);                                           // delete all items
+void sym_tab_free(sym_tab_t *t);                                            // destructor of symbol table
+data_type create_data_type(sym_tab_datatype first);                         // [linked list function] - first value of list of data types
+data_type add_data_type(data_type first, sym_tab_datatype value);           // [linked list function] - add more values to list of data types
+void delete_data_types(data_type *first);                                   // [linked list function] - delete all values from linked list of data types
+#endif                                                                      // __sym_tab_H__
