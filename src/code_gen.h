fb0fe4cef31fb96addd62c094db73dd1608ec595
LuKral <lukral72@seznam.cz>
2021-12-04 17:30:28 +0100

    add coments in header

--- src/code_gen.h
+++ src/code_gen.h
@@ -30,46 +30,64 @@ typedef enum {
 FILE* target;
 dynamic_string_t code;
 
-void get_target(FILE* file);
-
+//before generation
 void code_gen_init();
-void code_gen_free();
+
+ //after generation
+void get_target(FILE* file);
 void code_in_to_file();
-void add_code(char* inst);
-void add_code_int(int integer);
-void add_code_float(float integer);
+void code_gen_free();
 
-void generate_program_head();
-void generate_start_of_the_func(char* func_id);
-void generate_func_param_assign(char* param_id, int param_index);
-void generate_retval(int index, sym_tab_datatype type);
-void generate_assign_retval(int index, token_t* token);
-void generate_end_of_the_func(char* func_id);
-void generate_newframe();
-void generate_param_before_call(int index, token_t* param);
-void generate_call_of_the_func(char* func_id);
-void generate_after_call_var_assign(int index, sym_tab_datatype from_type, char* var_id, sym_tab_datatype to_type);
+//start of the program
+void generate_program_head(); //generates built in funcs etc.
 
-void generate_start_of_main();
-void generate_end_of_main();
+//definition of the function 
+void generate_start_of_the_func(char* func_id); //generates start of the function
+void generate_func_param_assign(char* param_id, int param_index); // generates declaration and init of the parameters of the function
+void generate_retval(int index, sym_tab_datatype type); //generates declaration of the retval
+//here is the CODE of the function//
+void generate_assign_retval(int index); //generates retval assign
+void generate_end_of_the_func(char* func_id);// generates the end of the function
+//
 
+//
+void generate_start_of_main();//generates the start of the main func
+//
+
+//call of the function 
+void generate_newframe(); // generates newframe before passing the parametrs of the function
+void generate_param_before_call(int index, token_t* param); //generates the passing of the parameters before call
+void generate_call_of_the_func(char* func_id); // calls the function
+void generate_after_call_var_assign(int index, sym_tab_datatype from_type, char* var_id, sym_tab_datatype to_type); //generates the assign of the retval 
+//
+
+//
+void generate_end_of_main();// generates the end of the main func
+//
+
+//expresion generation
+void generate_push(token_t* token);//generates push of the value on the stack
+void generate_pop(token_t* token); //generates pop of the value from the stack
+void generate_declare_variable(char* var_id); //generates declaration of the variable
+void generate_init_variable(char* var_id, sym_tab_datatype type);//generates init of the variable
+void generate_operation(rule_t rule);//generates the operation according the rule
+//
+
+//type check generation
 void generate_type_check_before_asign(int index, sym_tab_datatype from_type, char* var_id, sym_tab_datatype to_type);
 void generate_type_check_before_operation(char* var_id1, sym_tab_datatype type1, char* var_id2, sym_tab_datatype type2);
+//
 
-void generate_default_variable_value(sym_tab_datatype type);
-void generate_operand(token_t* operand);
-void generate_operation(rule_t rule);
-
+//if generation
 void generate_start_of_if(int if_index);
 void generate_start_of_else(int else_index, int if_index);
 void generate_end_of_else(int else_index);
 void generate_end_of_if(int if_index);
+//
 
-void generate_push(token_t* token);
-void generate_pop(token_t* token);
-void generate_declare_variable(char* var_id);
-void generate_init_variable(char* var_id, sym_tab_datatype type);
 
+
+//auxiliary functions
 void generate_function_chr();
 void generate_function_ord();
 void generate_function_substr();
@@ -78,6 +96,12 @@ void generate_function_readn();
 void generate_function_readi();
 void generate_function_reads();
 void generate_built_in_funcs();
+void generate_default_variable_value(sym_tab_datatype type);
+void add_code(char* inst);
+void add_code_int(int integer);
+void add_code_float(float integer);
+void generate_operand(token_t* operand);;
+//
 
 
 
